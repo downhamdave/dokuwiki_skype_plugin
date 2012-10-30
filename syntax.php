03cecfd0b6d4e041d92937024256f3bbec61a8c4
tschinz <zahno.silvan@gmail.com>
2012-10-30 07:39:19 +0100

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -39,7 +39,7 @@ class syntax_plugin_skype extends DokuWiki_Syntax_Plugin {
         return array(
             'author' => 'Zahno Silvan',
             'email'  => 'zaswiki@gmail.com',
-            'date'   => '2012-06-14',
+            'date'   => '2012-10-22',
             'name'   => 'Skype Plugin',
             'desc'   => 'Skype Button',
             'url'    => 'http://zawiki.dyndns.org/doku.php/tschinz:dw_skype',
@@ -65,7 +65,7 @@ class syntax_plugin_skype extends DokuWiki_Syntax_Plugin {
      * Connect pattern to lexer
      */
     function connectTo($mode) {
-      $this->Lexer->addSpecialPattern('\{\{skype>.*?\}\}',$mode,'plugin_skype');
+        $this->Lexer->addSpecialPattern('\{\{skype>.*?\}\}',$mode,'plugin_skype');
     }
 
     /**
@@ -92,71 +92,70 @@ class syntax_plugin_skype extends DokuWiki_Syntax_Plugin {
      * Create output
      */
     function render($mode, &$renderer, $data) {
-		if($mode == 'xhtml'){
+        if($mode == 'xhtml'){
+            $options['function']  = $this->getConf('function'); // default value
+            $options['size']      = $this->getConf('size');
+            $options['content']   = $this->getConf('content');
+            $options['style']     = $this->getConf('style');
 
-			$options['function']  = $this->getConf('function'); // default value
-			$options['size']      = $this->getConf('size');
-			$options['content']   = $this->getConf('content');
-			$options['style']     = $this->getConf('style');
-
-			// strip {{skype> from start
-			$data     = substr($data,8);
-			// strip }} from end
-			$data     = substr($data,0,-2);
+           // strip {{skype> from start
+           $data     = substr($data,8);
+           // strip }} from end
+           $data     = substr($data,0,-2);
       
-      //get function and username
-      $var1='';
-      $var2='';
-      list($var1, $var2) = explode(',', $data, 2);
+           //get function and username
+           $var1='';
+           $var2='';
+           list($var1, $var2) = explode(',', $data, 2);
       
-      if ($var1 == 'chat' or $var1 == 'add' or $var1 == 'call' or $var1 == 'userinfo' or $var1 == 'voicemail' or $var1 == 'sendfile')
-      {
-        $options['function'] = $var1;
-        $data = $var2;
-      }
-      elseif ($var2 == 'chat' or $var2 == 'add' or $var2 == 'call' or $var2 == 'userinfo' or $var2 == 'voicemail' or $var2 == 'sendfile')
-      {
-        $options['function'] = $var2;
-        $data = $var1;
-      }
-      else
-      {
-        $data = $var1;
-      }
+           if ($var1 == 'chat' or $var1 == 'add' or $var1 == 'call' or $var1 == 'userinfo' or $var1 == 'voicemail' or $var1 == 'sendfile')
+           {
+               $options['function'] = $var1;
+               $data = $var2;
+           }
+           elseif ($var2 == 'chat' or $var2 == 'add' or $var2 == 'call' or $var2 == 'userinfo' or $var2 == 'voicemail' or $var2 == 'sendfile')
+           {
+               $options['function'] = $var2;
+               $data = $var1;
+           }
+           else
+           {
+               $data = $var1;
+           }
       
-      if (empty($data))
-			{
-				$renderer->doc .= 'No skype name given';
-				return true;
-			}
+           if (empty($data))
+           {
+               $renderer->doc .= 'No skype name given';
+               return true;
+           }
 
-			$code = '<script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script>';
-			$code .= '<a href="skype:'.$data.'?'.$options['function'].'">';
+           $code = '<script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script>';
+           $code .= '<a href="skype:'.$data.'?'.$options['function'].'">';
 
-			if($options['style'] == 'balloon') {
-				$code .= '<img src="http://mystatus.skype.com/balloon/'.$data.'" style="border: none;" width="150" height="60" alt="My status" /></a>';
-			}
-			elseif($options['content'] == 'icon+text') {
-					if($options['size'] == 'big'){
-						$code .= '<img src="http://mystatus.skype.com/bigclassic/'.$data.'" style="border: none;" width="182" height="44" alt="My status" /></a>';
-					}
-					else {
-						$code .= '<img src="http://mystatus.skype.com/smallclassic/'.$data.'" style="border: none;" width="114" height="20" alt="My status" /></a>';
-					}
-				}
-				else {
-					if($options['size'] == 'big') {
-						$code .= '<img src="http://mystatus.skype.com/mediumicon/'.$data.'" style="border: none;" width="26" height="26" alt="My status" /></a>';
-					}
-					else {
-						$code .= '<img src="http://mystatus.skype.com/smallicon/'.$data.'" style="border: none;" width="16" height="16" alt="My status" /></a>';
-					}
-				}
-			
-			$renderer->doc .= $code;
+           if($options['style'] == 'balloon') {
+               $code .= '<img src="http://mystatus.skype.com/balloon/'.$data.'" style="border: none;" width="150" height="60" alt="My status" /></a>';
+           }
+           elseif($options['content'] == 'icon+text') {
+               if($options['size'] == 'big'){
+                   $code .= '<img src="http://mystatus.skype.com/bigclassic/'.$data.'" style="border: none;" width="182" height="44" alt="My status" /></a>';
+                   }
+               else {
+                   $code .= '<img src="http://mystatus.skype.com/smallclassic/'.$data.'" style="border: none;" width="114" height="20" alt="My status" /></a>';
+               }
+           }
+           else {
+               if($options['size'] == 'big') {
+                   $code .= '<img src="http://mystatus.skype.com/mediumicon/'.$data.'" style="border: none;" width="26" height="26" alt="My status" /></a>';
+               }
+               else {
+                   $code .= '<img src="http://mystatus.skype.com/smallicon/'.$data.'" style="border: none;" width="16" height="16" alt="My status" /></a>';
+               }
+           }
+           
+           $renderer->doc .= $code;
 
-			return true;
+           return true;
         }
-        return false;
+    return false;
     }
 }
