586e9608ac3c0bded0b76a1f16664ef756874b88
downhamdave <live@monjo.com>
2016-05-14 19:55:08 +0100

    PHP7 compatibility

--- syntax.php
+++ syntax.php
@@ -71,7 +71,7 @@ class syntax_plugin_skype extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
           case DOKU_LEXER_ENTER :
             break;
@@ -91,7 +91,7 @@ class syntax_plugin_skype extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
         if($mode == 'xhtml'){
             $options['function']  = $this->getConf('function'); // default value
             $options['size']      = $this->getConf('size');
